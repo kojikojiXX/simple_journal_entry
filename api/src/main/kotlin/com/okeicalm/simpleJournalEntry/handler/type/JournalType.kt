142632ba9825fdb598e0b41ec96b2b1d31a516f1
nohara.koji <nohara.koji@moneyforward.co.jp>
2024-01-22 19:31:39 +0900

    feat: add user foreign key to JournalType

--- api/src/main/kotlin/com/okeicalm/simpleJournalEntry/handler/type/JournalType.kt
+++ api/src/main/kotlin/com/okeicalm/simpleJournalEntry/handler/type/JournalType.kt
@@ -1,5 +1,6 @@
 package com.okeicalm.simpleJournalEntry.handler.type
 
+import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
 import com.expediagroup.graphql.generator.annotations.GraphQLName
 import com.expediagroup.graphql.generator.scalars.ID
 import com.okeicalm.simpleJournalEntry.entity.Journal
@@ -12,10 +13,12 @@ data class JournalType(
     val id: ID,
     val incurredOn: LocalDate,
     val journalEntries: List<JournalEntryType>,
+    @GraphQLIgnore val userId: ID
 ) {
     constructor(journal: Journal) : this(
         id = ID(journal.id.toString()),
         incurredOn = journal.incurredOn,
-        journal.journalEntries.map { JournalEntryType(it) }
+        journal.journalEntries.map { JournalEntryType(it) },
+        ID(journal.userId.toString())
     )
 }
