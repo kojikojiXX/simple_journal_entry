3c960da809d12b56e865a904a59716eeb67a8b5f
nohara.koji <nohara.koji@moneyforward.co.jp>
2024-01-22 19:33:22 +0900

    feat: add userId to Journal entity

--- api/src/main/kotlin/com/okeicalm/simpleJournalEntry/entity/Journal.kt
+++ api/src/main/kotlin/com/okeicalm/simpleJournalEntry/entity/Journal.kt
@@ -4,16 +4,19 @@ import java.time.LocalDate
 
 data class Journal(
     val id: Long,
+    val userId: Long,
     val incurredOn: LocalDate,
     val journalEntries: List<JournalEntry>,
 ) {
     companion object {
         fun create(
+            userId: Long,
             incurredOn: LocalDate,
             journalEntries: List<JournalEntry>,
         ): Journal {
             return Journal(
                 id = 0,
+                userId = userId,
                 incurredOn = incurredOn,
                 journalEntries = journalEntries,
             )
