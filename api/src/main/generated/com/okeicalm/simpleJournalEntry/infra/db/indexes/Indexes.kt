fbe0be9fc3ee1b7daf261cda6360831d6a25e7d7
nohara.koji <nohara.koji@moneyforward.co.jp>
2024-01-23 11:40:53 +0900

    feat: this file was made automatically by change of journal database

--- api/src/main/generated/com/okeicalm/simpleJournalEntry/infra/db/indexes/Indexes.kt
+++ api/src/main/generated/com/okeicalm/simpleJournalEntry/infra/db/indexes/Indexes.kt
@@ -5,6 +5,7 @@ package com.okeicalm.simpleJournalEntry.infra.db.indexes
 
 
 import com.okeicalm.simpleJournalEntry.infra.db.tables.JournalEntries
+import com.okeicalm.simpleJournalEntry.infra.db.tables.Journals
 
 import org.jooq.Index
 import org.jooq.impl.DSL
@@ -18,3 +19,4 @@ import org.jooq.impl.Internal
 
 val JOURNAL_ENTRIES_FK_ACCOUNT: Index = Internal.createIndex(DSL.name("fk_account"), JournalEntries.JOURNAL_ENTRIES, arrayOf(JournalEntries.JOURNAL_ENTRIES.ACCOUNT_ID), false)
 val JOURNAL_ENTRIES_FK_JOURNAL: Index = Internal.createIndex(DSL.name("fk_journal"), JournalEntries.JOURNAL_ENTRIES, arrayOf(JournalEntries.JOURNAL_ENTRIES.JOURNAL_ID), false)
+val JOURNALS_FK_USER: Index = Internal.createIndex(DSL.name("fk_user"), Journals.JOURNALS, arrayOf(Journals.JOURNALS.USER_ID), false)
